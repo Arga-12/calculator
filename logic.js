676b54ce7e71b4a42b34127327cdc4fb9eae38e1
Arga-12 <argarill226@gmail.com>
2026-02-06 07:58:45 +0700

    obfuscated_js

--- logic.js
+++ logic.js
@@ -1,134 +1,213 @@
-const keys = document.querySelectorAll(".keyCalc");
-const print = document.getElementById("print");
-
-keys.forEach(tombol => {
-    tombol.addEventListener('click', () => {
-        const isiKey = tombol.textContent.trim();
-        
-        if (isiKey === "AC") {
-            print.value = "0";
-        } else if (isiKey === "Hapus") { 
-            print.value = print.value.slice(0, -1);
-        }
-        else if (isiKey === "Hitung!") { 
-            try {
-                let input = print.value;
-                let rumus = input.replace(/×/g, '*').replace(/÷/g, '/');
-                rumus = rumus.replace(/[^0-9+\-*/.()]/g, '');
-
-                if(rumus === '') {
-                    print.value = "0";
-                } else {
-                    let hasil = eval(rumus);
-                    History(input, hasil);
-                    print.value = hasil;
-                }
-            } catch (e) {
-                print.value = "WEIIIII";
-                console.error(e);
-            };
-        } else { 
-            const op = ['+', '-', '÷', '×', '.'];
-            let lastOp = print.value.slice(-1);
-            let allKeys = isiKey;
-
-            const spamKeys = op.includes(lastOp) && op.includes(allKeys);
-
-            if (spamKeys) {
-                print.value = print.value.slice(0, -1) + isiKey;
-            } else if (print.value === "0") {
-                print.value = isiKey;
-            } else {
-                print.value += isiKey;
-            };
-        };
-    })
-});
-
-//history func
-function History(rumus, hasil) {
-    let LiHistory = JSON.parse(localStorage.getItem("history")) || [];
-    
-    const data = {
-        ekspresi: rumus,
-        hasil: hasil,
-        date: new Date().toLocaleString()
-    };
-
-     LiHistory.unshift(data);
-     LiHistory = LiHistory.slice(0, 10);
-     localStorage.setItem("history", JSON.stringify(LiHistory));
-
-     console.log("Histori dibuat:", LiHistory);
-     renderHistory();
-};
-
-function renderHistory() {
-    const historyList = document.getElementById("historyList");
-    const LiHistory = JSON.parse(localStorage.getItem("history")) || [];
-
-    if (LiHistory.length === 0) {
-        historyList.innerHTML = "<p class='text-center text-gray-500'>Tidak ada history</p>";
-        return;
-    };
-
-    historyList.innerHTML = LiHistory.map((item, index) => `
-        <div class="flex items-center justify-between p-2 border-b border-gray-200">
-            <p>${index + 1}. ${item.ekspresi} = ${item.hasil}</p>
-            <p class="text-gray-500 text-sm">${item.date}</p>
-        </div>
-    `).join('');
-};
-
-function tampilHistory(rumus) {
-    print.value = rumus;
-};
-// tombol.addEventListener('click', () => {
-//     const a1 = Number(bil_1.value);
-//     const a2 = Number(bil_2.value);
-//     const o = op.value;
-
-//     if (o == "tambah") {
-//         hasil = a1 + a2;
-//     } else if (o == "kurang") {
-//         hasil = a1 - a2;
-//     } else if (o == "kali") {
-//         hasil = a1 * a2;
-//     } else if (o == "bagi") {
-//         hasil = a1 / a2;
-//     } else if (o == "mod") {
-//         hasil = a1 % a2;
-//     } else {
-//         print.textContent = `No numbers to operate rnow`;
-//     };
-
-//     print.textContent = `hasil:${hasil}`;
-// });
-
-// WEIIII
-const h = document.querySelectorAll(".wei");
-const wei = new Audio('uma/wei.mp3')
-wei.volume = 0.3
-wei.loop = true;
-
-const kagi = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value');
-
-Object.defineProperty(print, 'value', {
-    get: function () {
-        return kagi.get.call(this)
-    },
-    set: function (w) { 
-        kagi.set.call(this, w)
-        if (w === "WEIIIII") {
-            wei.play();
-            h[0].classList.remove('hidden');
-            h[1].classList.remove('hidden');
+const _0x2edac6 = _0x1ab9;
+function _0x5454() {
+  const _0x50420e = [
+    "addEventListener",
+    "uma/wei.mp3",
+    "ekspresi",
+    "getItem",
+    "classList",
+    "Hitung!",
+    "9jVQmhH",
+    "28xsFBCG",
+    "3029184YsBOpa",
+    "toLocaleString",
+    "11432090CdDoox",
+    "volume",
+    "add",
+    "hidden",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22flex\x20items-center\x20justify-between\x20p-2\x20border-b\x20border-gray-200\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p>",
+    "history",
+    "8777700erFbcF",
+    "set",
+    "forEach",
+    "1735998BqLoog",
+    "1146844RjGuwy",
+    "setItem",
+    "get",
+    "getElementById",
+    "<p\x20class=\x27text-center\x20text-gray-500\x27>Tidak\x20ada\x20history</p>",
+    "getOwnPropertyDescriptor",
+    "length",
+    "prototype",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22text-gray-500\x20text-sm\x22>",
+    "45870EstLpd",
+    "error",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20",
+    "\x20=\x20",
+    "log",
+    "trim",
+    "2VwFnvO",
+    "Hapus",
+    "parse",
+    "unshift",
+    "defineProperty",
+    "includes",
+    "querySelectorAll",
+    "map",
+    "Histori\x20dibuat:",
+    "value",
+    "replace",
+    ".keyCalc",
+    "call",
+    "stringify",
+    "historyList",
+    "hasil",
+    "remove",
+    "869316rJcLdi",
+    "WEIIIII",
+    "slice",
+  ];
+  _0x5454 = function () {
+    return _0x50420e;
+  };
+  return _0x5454();
+}
+function _0x1ab9(_0x11b44a, _0x4e0a49) {
+  _0x11b44a = _0x11b44a - 0xf6;
+  const _0x545428 = _0x5454();
+  let _0x1ab9a2 = _0x545428[_0x11b44a];
+  return _0x1ab9a2;
+}
+(function (_0x47b7ff, _0x124416) {
+  const _0x517dfc = _0x1ab9,
+    _0x23bc05 = _0x47b7ff();
+  while (!![]) {
+    try {
+      const _0x324168 =
+        (-parseInt(_0x517dfc(0x112)) / 0x1) *
+          (-parseInt(_0x517dfc(0x123)) / 0x2) +
+        (-parseInt(_0x517dfc(0x12c)) / 0x3) *
+          (parseInt(_0x517dfc(0x103)) / 0x4) +
+        parseInt(_0x517dfc(0x10c)) / 0x5 +
+        (-parseInt(_0x517dfc(0x102)) / 0x6) *
+          (parseInt(_0x517dfc(0xf6)) / 0x7) +
+        -parseInt(_0x517dfc(0xf7)) / 0x8 +
+        parseInt(_0x517dfc(0xff)) / 0x9 +
+        parseInt(_0x517dfc(0xf9)) / 0xa;
+      if (_0x324168 === _0x124416) break;
+      else _0x23bc05["push"](_0x23bc05["shift"]());
+    } catch (_0x12f1fd) {
+      _0x23bc05["push"](_0x23bc05["shift"]());
+    }
+  }
+})(_0x5454, 0x92b36);
+const keys = document[_0x2edac6(0x118)](_0x2edac6(0x11d)),
+  print = document["getElementById"]("print");
+keys[_0x2edac6(0x101)]((_0x2f2cc8) => {
+  const _0x48ef5c = _0x2edac6;
+  _0x2f2cc8[_0x48ef5c(0x126)]("click", () => {
+    const _0x21bbf5 = _0x48ef5c,
+      _0x172459 = _0x2f2cc8["textContent"][_0x21bbf5(0x111)]();
+    if (_0x172459 === "AC") print[_0x21bbf5(0x11b)] = "0";
+    else {
+      if (_0x172459 === _0x21bbf5(0x113))
+        print[_0x21bbf5(0x11b)] = print[_0x21bbf5(0x11b)][_0x21bbf5(0x125)](
+          0x0,
+          -0x1,
+        );
+      else {
+        if (_0x172459 === _0x21bbf5(0x12b)) {
+          try {
+            let _0x2b4886 = print[_0x21bbf5(0x11b)],
+              _0x2a2580 = _0x2b4886[_0x21bbf5(0x11c)](/×/g, "*")[
+                _0x21bbf5(0x11c)
+              ](/÷/g, "/");
+            _0x2a2580 = _0x2a2580[_0x21bbf5(0x11c)](/[^0-9+\-*/.()]/g, "");
+            if (_0x2a2580 === "") print[_0x21bbf5(0x11b)] = "0";
+            else {
+              let _0x2c66f2 = eval(_0x2a2580);
+              (History(_0x2b4886, _0x2c66f2),
+                (print[_0x21bbf5(0x11b)] = _0x2c66f2));
+            }
+          } catch (_0x1ee940) {
+            ((print["value"] = _0x21bbf5(0x124)),
+              console[_0x21bbf5(0x10d)](_0x1ee940));
+          }
         } else {
-            wei.pause();
-            h[0].classList.add('hidden');
-            h[1].classList.add('hidden');
+          const _0x4f62d3 = ["+", "-", "÷", "×", "."];
+          let _0xfe594d = print[_0x21bbf5(0x11b)][_0x21bbf5(0x125)](-0x1),
+            _0x303075 = _0x172459;
+          const _0x48386a =
+            _0x4f62d3[_0x21bbf5(0x117)](_0xfe594d) &&
+            _0x4f62d3[_0x21bbf5(0x117)](_0x303075);
+          if (_0x48386a)
+            print["value"] =
+              print[_0x21bbf5(0x11b)]["slice"](0x0, -0x1) + _0x172459;
+          else
+            print[_0x21bbf5(0x11b)] === "0"
+              ? (print[_0x21bbf5(0x11b)] = _0x172459)
+              : (print[_0x21bbf5(0x11b)] += _0x172459);
         }
-    },
-    configurable: true,
-    enumerable: true
-})
+      }
+    }
+  });
+});
+function History(_0x172348, _0x2b51ba) {
+  const _0x26f1de = _0x2edac6;
+  let _0x5dc39d =
+    JSON[_0x26f1de(0x114)](localStorage["getItem"](_0x26f1de(0xfe))) || [];
+  const _0x4654bf = {
+    ekspresi: _0x172348,
+    hasil: _0x2b51ba,
+    date: new Date()[_0x26f1de(0xf8)](),
+  };
+  (_0x5dc39d[_0x26f1de(0x115)](_0x4654bf),
+    (_0x5dc39d = _0x5dc39d["slice"](0x0, 0xa)),
+    localStorage[_0x26f1de(0x104)](
+      _0x26f1de(0xfe),
+      JSON[_0x26f1de(0x11f)](_0x5dc39d),
+    ),
+    console[_0x26f1de(0x110)](_0x26f1de(0x11a), _0x5dc39d),
+    renderHistory());
+}
+function renderHistory() {
+  const _0x200990 = _0x2edac6,
+    _0x59bf40 = document[_0x200990(0x106)](_0x200990(0x120)),
+    _0x4bd9e0 = JSON["parse"](localStorage[_0x200990(0x129)]("history")) || [];
+  if (_0x4bd9e0[_0x200990(0x109)] === 0x0) {
+    _0x59bf40["innerHTML"] = _0x200990(0x107);
+    return;
+  }
+  _0x59bf40["innerHTML"] = _0x4bd9e0[_0x200990(0x119)](
+    (_0x4900a0, _0x1fd1ae) =>
+      _0x200990(0xfd) +
+      (_0x1fd1ae + 0x1) +
+      ".\x20" +
+      _0x4900a0[_0x200990(0x128)] +
+      _0x200990(0x10f) +
+      _0x4900a0[_0x200990(0x121)] +
+      _0x200990(0x10b) +
+      _0x4900a0["date"] +
+      _0x200990(0x10e),
+  )["join"]("");
+}
+function tampilHistory(_0x44abf5) {
+  const _0x1b6d19 = _0x2edac6;
+  print[_0x1b6d19(0x11b)] = _0x44abf5;
+}
+const h = document["querySelectorAll"](".wei"),
+  wei = new Audio(_0x2edac6(0x127));
+((wei[_0x2edac6(0xfa)] = 0.3), (wei["loop"] = !![]));
+const kagi = Object[_0x2edac6(0x108)](
+  HTMLInputElement[_0x2edac6(0x10a)],
+  _0x2edac6(0x11b),
+);
+Object[_0x2edac6(0x116)](print, _0x2edac6(0x11b), {
+  get: function () {
+    const _0x23e62c = _0x2edac6;
+    return kagi[_0x23e62c(0x105)]["call"](this);
+  },
+  set: function (_0x4c0efc) {
+    const _0x8123a5 = _0x2edac6;
+    (kagi[_0x8123a5(0x100)][_0x8123a5(0x11e)](this, _0x4c0efc),
+      _0x4c0efc === _0x8123a5(0x124)
+        ? (wei["play"](),
+          h[0x0]["classList"][_0x8123a5(0x122)](_0x8123a5(0xfc)),
+          h[0x1][_0x8123a5(0x12a)][_0x8123a5(0x122)]("hidden"))
+        : (wei["pause"](),
+          h[0x0][_0x8123a5(0x12a)][_0x8123a5(0xfb)](_0x8123a5(0xfc)),
+          h[0x1][_0x8123a5(0x12a)][_0x8123a5(0xfb)](_0x8123a5(0xfc))));
+  },
+  configurable: !![],
+  enumerable: !![],
+});
